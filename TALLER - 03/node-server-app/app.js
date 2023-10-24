b9faa19548bae99c4df2c6fbdc3ca89ebfbccbca
jhon.guzman <jhon.guzman@rappi.com>
2023-10-24 12:10:46 -0500

    Catfact API

---
+++ TALLER - 03/node-server-app/app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+
+const app = express()
+const port = 8080
+const randomCatFacts = [
+    "On average, a cat will sleep for 16 hours a day.",
+    "Purring does not always indicate that a cat is happy and healthy - some cats will purr loudly when they are terrified or in pain.",
+    "An adult lion's roar can be heard up to five miles (eight kilometers) away.",
+    "Milk can give some cats diarrhea.",
+    "Cats have 30 teeth (12 incisors, 10 premolars, 4 canines, and 4 molars), while dogs have 42. Kittens have baby teeth, which are replaced by permanent teeth around the age of 7 months.",
+    "Many cats cannot properly digest cow's milk. Milk and milk products give them diarrhea.",
+    "Owning a cat is actually proven to be beneficial for your health.",
+    "The Cat Fanciers Association (CFA) recognizes 44 breeds of cats.",
+    "Most cats had short hair until about 100 years ago, when it became fashionable to own cats and experiment with breeding."
+]
+
+app.get("/api/catFact", (req, res) => {
+    const randomCatFact = randomCatFacts[Math.floor(Math.random()*randomCatFacts.length)]
+
+    res.status(200).json({fact: randomCatFact})
+})
+
+app.listen(port, "0.0.0.0", ()=> {
+    console.log("Servidor ejecutandose en el puerto: ", port)
+})
